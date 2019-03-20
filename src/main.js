c1b79b756c939f376caa6fc2091d1c5395d2173f
renato.medeiros <renato.medeiros@b2eitconsulting.com.br>
2019-03-19 23:03:07 -0300

    ajustes gerais

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router';
 import NowUiKit from './plugins/now-ui-kit';
 import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
+//import awsmobile from './aws-exports'
+//Amplify.configure(awsmobile)
 
 Vue.config.productionTip = false;
 
